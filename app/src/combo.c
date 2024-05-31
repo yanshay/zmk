06faeec6ec125e070e4d1e5c807c71dd38f0cc0f
yanshay <yanivshaya@gmail.com>
2024-05-31 23:22:01 +0300

    fix combo not interrupting tap-dance (and maybe more)

--- app/src/combo.c
+++ app/src/combo.c
@@ -296,6 +296,12 @@ static inline int press_combo_behavior(struct combo_cfg *combo, int32_t timestam
 
     last_combo_timestamp = timestamp;
 
+    ZMK_EVENT_RAISE(new_zmk_position_state_changed(
+        (struct zmk_position_state_changed){.source = 0, // not ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
+                                            .state = true,
+                                            .position = -1, // not combo->virtual_key_position,
+                                            .timestamp = timestamp}));
+
     return behavior_keymap_binding_pressed(&combo->behavior, event);
 }
 
@@ -304,6 +310,12 @@ static inline int release_combo_behavior(struct combo_cfg *combo, int32_t timest
         .position = combo->virtual_key_position,
         .timestamp = timestamp,
     };
+
+    ZMK_EVENT_RAISE(new_zmk_position_state_changed(
+        (struct zmk_position_state_changed){.source = 0, // not ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
+                                            .state = false,
+                                            .position = -1, // not combo->virtual_key_position,
+                                            .timestamp = timestamp}));
 
     return behavior_keymap_binding_released(&combo->behavior, event);
 }
